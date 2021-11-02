ee00c846b19c2b59275ebd8950e1071b2723d282
kbberker <kbbuk90@gmail.com>
2021-11-02 19:47:32 +0000

    feat: accept file input and log contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [listOfSiteVisits, setListOfSiteVisits] = useState<string[]>([]);
+
+  console.log({ listOfSiteVisits });
+
+  const fileReader = new FileReader();
+  let fileContents: string;
+
+  const readFileInput = () => {
+    console.log('clicked');
+    const fileList = fileInputRef!.current;
+
+    if (fileList && fileList.files) {
+      fileReader.readAsText(fileList!.files[0]);
+    }
+
+    fileReader.onloadend = () => {
+      fileContents = fileReader.result!.toString();
+      console.log(fileContents.split(/[\r\n]+/));
+      setListOfSiteVisits(fileContents.split(/[\r\n]+/));
+    };
+  };
+
   return (
     <div className="App">
       <label htmlFor="file-input">
         Please choose your log file:
-        <input type="file" name="file-input" accept=".log" />
+        <input ref={fileInputRef} type="file" name="file-input" accept=".log" />
       </label>
       <div>
-        <button type="button">Submit</button>
+        <button type="button" onClick={readFileInput}>Submit</button>
       </div>
     </div>
   );
